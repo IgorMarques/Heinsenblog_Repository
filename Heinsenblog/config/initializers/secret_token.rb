31561906e641a36bedfd5bc2d4e0a904f2c95968
Igor Marques <igormarquessilva@gmail.com>
2012-08-12 13:21:26 -0300

    Criação do Blog

---
+++ Heinsenblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heinsenblog::Application.config.secret_token = '46009628997bee56f2cdad5e1a933547834b34ba056578e2d7b5f671f3c770506d298707be91ebf2e601d9a82056cfaca6e4eed877c9201f1de615701d9866e7'
